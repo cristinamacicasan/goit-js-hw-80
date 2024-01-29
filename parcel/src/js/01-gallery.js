af6067b23d7b4da53716bdcc7327a83ee6e4be31
cristinamacicasan <cmacicasan@gmail.com>
2024-01-29 09:06:06 +0200

    finish

--- parcel/src/js/01-gallery.js
+++ parcel/src/js/01-gallery.js
@@ -2,4 +2,47 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+function galleryElementCreation(item) {
+  const galleryItem = document.createElement('li');
+  const galleryLink = document.createElement('a');
+  const galleryImage = document.createElement('img');
+  galleryItem.classList.add('gallery__item');
+  galleryLink.classList.add('gallery__link');
+  galleryImage.classList.add('gallery__image');
+  galleryLink.href = item.original;
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.dataset.source = item.original; 
+  galleryItem.appendChild(galleryLink);
+  galleryLink.appendChild(galleryImage);
+  return galleryItem;
+}
+
+
+
+galleryItems.forEach(item => {
+  const galleryItem = galleryElementCreation(item);
+  galleryList.appendChild(galleryItem);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", (clickEvent) => {
+  clickEvent.preventDefault();
+  lightbox.open();
+});
+
+document.addEventListener('keydown', (escEvent) => {
+  if (escEvent.key === 'Escape' && lightbox.isOpen) {
+    lightbox.close();
+  }
+});
--- parcel/src/js/02-video.js
+++ parcel/src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+const saveTimeThrottled = throttle(saveCurrentTime, 1000);
+player.on('timeupdate', saveTimeThrottled);
+function saveCurrentTime(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+window.addEventListener('load', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+});
